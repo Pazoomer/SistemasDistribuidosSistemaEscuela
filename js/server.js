b7b0102c4de4818d29f6514160904952ba1baae8
Jorge Zamora <jorge.zamora245103@potros.itson.edu.mx>
2025-04-24 15:52:46 -0700

    quitar import

--- js/server.js
+++ js/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2');
 const { parse } = require('url');
-const { console } = require('console');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
