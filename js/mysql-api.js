b3cd8073b1047be84b858ca3b945a35f5da0473e
Jorge Zamora <jorge.zamora245103@potros.itson.edu.mx>
2025-05-01 10:53:44 -0700

    Cambio de local host a url Railway

--- js/mysql-api.js
+++ js/mysql-api.js
@@ -1,11 +1,11 @@
 export async function cargarDatos() {
-    const res = await fetch('http://localhost:3000/maestros');
+    const res = await fetch('https://sistemasdistribuidossistemaescuela.onrender.com/maestros');
     const data = await res.json();
     return data;
   }
   
   export async function agregarMaestro(datos) {
-    const res = await fetch('http://localhost:3000/maestros', {
+    const res = await fetch('https://sistemasdistribuidossistemaescuela.onrender.com/maestros', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(datos)
